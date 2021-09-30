c5d7a845ce10dd90b9b69c31a59e22eadaa379eb
v1rtl <pilll.pl22@gmail.com>
2021-09-30 17:51:08 +0300

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# miller_rabin
+
+Modern rewrite of [miller-rabin](https://github.com/indutny/miller-rabin).
+
+```ts
+import { MillerRabin } from 'https://deno.land/x/miller_rabin/mod.ts'
+
+const md = new MillerRabin()
+```
---
+++ mod.ts
@@ -0,0 +1,106 @@
+import { default as brorand, Rand } from 'https://cdn.skypack.dev/brorand'
+import { BN } from 'https://deno.land/x/bn_deno@1.0.0/lib/bn.js'
+
+export class MillerRabin {
+  rand: any
+  constructor(rand?: typeof Rand) {
+    this.rand = rand || new brorand.Rand()
+  }
+
+  static create(rand: typeof Rand) {
+    return new MillerRabin(rand)
+  }
+
+  _randbelow(n: BN) {
+    const len = n.bitLength()
+    const min_bytes = Math.ceil(len / 8)
+
+    // Generage random bytes until a number less than n is found.
+    // This ensures that 0..n-1 have an equal probability of being selected.
+    let a: BN
+    do a = new BN(this.rand.generate(min_bytes))
+    while (a.cmp(n) >= 0)
+
+    return a
+  }
+
+  _randrange(start: BN, stop: BN): BN {
+    // Generate a random number greater than or equal to start and less than stop.
+    const size = stop.sub(start)
+    return start.add(this._randbelow(size))
+  }
+
+  test(n: BN, k: number, cb: (a: any) => void): boolean {
+    const len = n.bitLength()
+    const red = BN.mont(n)
+    const rone = new BN(1).toRed(red)
+
+    if (!k) k = Math.max(1, (len / 48) | 0)
+
+    // Find d and s, (n - 1) = (2 ^ s) * d;
+    const n1 = n.subn(1)
+    for (var s = 0; !n1.testn(s); s++) {}
+    const d = n.shrn(s)
+
+    const rn1 = n1.toRed(red)
+
+    const prime = true
+    for (; k > 0; k--) {
+      const a = this._randrange(new BN(2), n1)
+      if (cb) cb(a)
+
+      let x = a.toRed(red).redPow(d)
+      if (x.cmp(rone) === 0 || x.cmp(rn1) === 0) continue
+
+      for (var i = 1; i < s; i++) {
+        x = x.redSqr()
+
+        if (x.cmp(rone) === 0) return false
+        if (x.cmp(rn1) === 0) break
+      }
+
+      if (i === s) return false
+    }
+
+    return prime
+  }
+
+  getDivisor(n: BN, k: number): BN | boolean {
+    const len = n.bitLength()
+    const red = BN.mont(n)
+    const rone = new BN(1).toRed(red)
+
+    if (!k) k = Math.max(1, (len / 48) | 0)
+
+    // Find d and s, (n - 1) = (2 ^ s) * d;
+    const n1 = n.subn(1)
+    for (var s = 0; !n1.testn(s); s++) {}
+    const d = n.shrn(s)
+
+    const rn1 = n1.toRed(red)
+
+    for (; k > 0; k--) {
+      const a = this._randrange(new BN(2), n1)
+
+      const g = n.gcd(a)
+      if (g.cmpn(1) !== 0) return g
+
+      let x = a.toRed(red).redPow(d)
+      if (x.cmp(rone) === 0 || x.cmp(rn1) === 0) continue
+
+      for (var i = 1; i < s; i++) {
+        x = x.redSqr()
+
+        if (x.cmp(rone) === 0) return x.fromRed().subn(1).gcd(n)
+        if (x.cmp(rn1) === 0) break
+      }
+
+      if (i === s) {
+        x = x.redSqr()
+        return x.fromRed().subn(1).gcd(n)
+      }
+    }
+
+    return false
+  }
+}
